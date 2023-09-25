fd7b9a8ef887ea0c1b6ce20fd78a8abe9aca000a
Samson Cheung <samsonc89@yahoo.com>
2023-09-25 13:30:10 -0700

    Fix styling to CTA button in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,16 +14,21 @@ export const Hero = () => {
       <h1 className="mt-32 text-3xl">
         Unlimited email design. <br /> One low-priced monthly rate.
       </h1>
-      <p className="text-xl tracking-wider text-gray-300/80">
+      <p className="mb-10 text-xl tracking-wider text-gray-300/80">
         With Limitless, you can get unlimited email design requests, and
         revisions for{" "}
         <span className="font-semibold leading-7 text-white underline decoration-orange-300 decoration-4 underline-offset-4">
           70% less than the cost of a full-time designer.
         </span>
       </p>
-      <button>See our plans</button>
-      <p>
-        <span className=" pulse-dot text-3xl text-white opacity-100">•</span>
+      <a
+        href="#pricing"
+        className="my-6 rounded-md bg-white px-6 py-3 text-lg font-semibold text-black"
+      >
+        See our plans
+      </a>
+      <p className="my-5">
+        <span className="pulse-dot text-3xl text-white opacity-100">•</span>
         <span className=" pulse-dot text-3xl text-white opacity-100">•</span>
         <span className=" pulse-dot text-3xl text-white opacity-100">•</span>
         <span className=" pulse-dot text-3xl text-white opacity-100">•</span>
