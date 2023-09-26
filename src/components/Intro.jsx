de3325f56589544e371b5e7f990cf24c963a681c
Samson Cheung <samsonc89@yahoo.com>
2023-09-26 10:12:17 -0700

    Add gradient text styling

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,18 +4,24 @@ export const Intro = () => {
   return (
     <section className="intro container">
       <img src={image} className="rounded-md" />
-      <div>
-        <h2>
-          Finding a reliable freelancer is really hard. Hiring a full-time
-          designer is too expensive.
+      <div className="my-12 px-14 text-center">
+        <h2 className="text-3xl">
+          {
+            "Finding a reliable freelancer is really hard. Hiring a full-time designer is too expensive. "
+          }
+          {/* <span className="bg-gradient-to-r from-blue-600 to-pink-500 bg-clip-text text-transparent"> */}
+          <span className="text-gradient block">
+            Limitless makes email <span className="inline-block">easy</span>
+          </span>
         </h2>
-        <p>Limitless makes email easy</p>
         <p>
           Our high-quality email design service is stress-free and affordable.
           We give you access to world-class design, unlimited projects, and
           limitless revisions for one flat monthly fee.
         </p>
-        <a href="">How it works</a>
+        <a href="#how-it-works" className="button">
+          How it works
+        </a>
       </div>
     </section>
   )
