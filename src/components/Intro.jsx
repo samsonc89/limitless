44e4c764204197697fea93449d08691f20090e6b
Samson Cheung <samsonc89@yahoo.com>
2023-09-26 09:53:00 -0700

    Add all conttent

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,3 +1,22 @@
+import image from "../assets/intro-img.webp"
+
 export const Intro = () => {
-  return <>Intro</>
+  return (
+    <section className="intro container">
+      <img src={image} className="rounded-md" />
+      <div>
+        <h2>
+          Finding a reliable freelancer is really hard. Hiring a full-time
+          designer is too expensive.
+        </h2>
+        <p>Limitless makes email easy</p>
+        <p>
+          Our high-quality email design service is stress-free and affordable.
+          We give you access to world-class design, unlimited projects, and
+          limitless revisions for one flat monthly fee.
+        </p>
+        <a href="">How it works</a>
+      </div>
+    </section>
+  )
 }
