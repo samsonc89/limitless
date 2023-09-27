277e2106beb5d668923fa11dff417bf243aaee56
Samson Cheung <samsonc89@yahoo.com>
2023-09-27 08:47:44 -0700

    Fix spacing for larger screens

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,15 +3,15 @@ import image from "../assets/intro-img.webp"
 export const Intro = () => {
   return (
     <section className="intro container ">
-      <div className="gap-5 lg:grid lg:grid-cols-2">
-        <img src={image} className="shrink-0 rounded-t-md lg:order-2" />
-        <div className="rounded-b-md bg-white px-12 py-12 text-center lg:text-start">
+      <div className="grid-cols-2 gap-5 xl:grid">
+        <img src={image} className=" shrink-0 rounded-t-md xl:order-2" />
+        <div className="rounded-b-md bg-white px-12 py-12 text-center xl:text-start">
           <h2 className="text-3xl tracking-wide  ">
             {
               "Finding a reliable freelancer is really hard. Hiring a full-time designer is too expensive. "
             }
             {/* <span className="bg-gradient-to-r from-blue-600 to-pink-500 bg-clip-text text-transparent"> */}
-            <span className="text-gradient block md:mt-2">
+            <span className="text-gradient block md:mt-2 lg:max-xl:mx-auto lg:max-xl:w-1/2">
               Limitless makes email <span className="inline-block">easy</span>
             </span>
           </h2>
