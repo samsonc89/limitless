f3836facf3a8284c9fcdb3d5d105ee54d886807e
Samson Cheung <samsonc89@yahoo.com>
2023-09-27 09:37:32 -0700

    Fix spacing for large screens

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -28,17 +28,28 @@ export const Benefits = () => {
   ]
 
   return (
-    <section className="container rounded-md bg-white">
-      <p>LIMITLESS SUBSCRIPTION BENEFITS</p>
-      <h2>Unlimited creativity. Unbelievable price. Unbeatable turnaround.</h2>
-      <a>Choose your plan</a>
-      <div className="xl:flex">
+    <section className="container rounded-md bg-white py-12 text-center">
+      <p className="text-gradient mb-5 block tracking-wider">
+        LIMITLESS SUBSCRIPTION BENEFITS
+      </p>
+      <h2 className="mx-auto mb-8 text-3xl tracking-wider lg:w-3/5 xl:w-1/2">
+        Unlimited creativity. Unbelievable price. Unbeatable turnaround.
+      </h2>
+      <a href="#pricing" className="button">
+        Choose your plan
+      </a>
+      <div className=" xl:flex">
         {content.map((item, i) => {
           return (
-            <div key={item[i]}>
+            <div
+              key={item[i]}
+              className="my-14 grid grid-cols-1 place-items-center gap-6"
+            >
               <img src={item.icon} />
-              <h3>{item.heading}</h3>
-              <p>{item.text}</p>
+              <div>
+                <h3 className="mb-2 px-5 text-2xl">{item.heading}</h3>
+                <p className="mx-auto w-4/5 text-lg font-light">{item.text}</p>
+              </div>
             </div>
           )
         })}
