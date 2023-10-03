b37961ce99ced6625947a13092bf007ef94f6fc4
Samson Cheung <samsonc89@yahoo.com>
2023-10-03 10:39:08 -0700

    Add Testimonial Card component

---
+++ src/components/Mybite.jsx
@@ -0,0 +1,10 @@
+export const Mybite = () => {
+  return (
+    <div>
+      <div>Testimonial 1</div>
+      <div>
+        <img src="" alt="" />g
+      </div>
+    </div>
+  )
+}
---
+++ src/components/TestimonialCard.jsx
@@ -0,0 +1,12 @@
+export const TestimonialCard = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <p>Text</p>
+      <p>Name</p>
+      <p>Position</p>
+    </div>
+  )
+}
