72fcfed14ca54412e21cb1a8ac3d3a58b7cab023
Samson Cheung <samsonc89@yahoo.com>
2023-10-03 10:45:34 -0700

    Add images to top testimonials

--- src/components/Testimonial1.jsx
+++ src/components/Testimonial1.jsx
@@ -1,13 +1,17 @@
+import adam from "../assets/adam.webp"
+import bart from "../assets/bart.webp"
+import { TestimonialCard } from "./TestimonialCard"
+
 export const Testimonial1 = () => {
   const content = [
     {
-      img: "",
+      img: adam,
       text: "Yes they do beautiful design. Yes they write squeaky-clean code. But the thing that makes them so special is how darn easy they are to work with. Can you have it all? Apparently with Limitless.",
       name: "Adam Schwartz",
       position: "Founder, Articulate",
     },
     {
-      img: "",
+      img: bart,
       text: "Limitless continues to deliver clean, elegant design—perfectly executed and always on time.",
       name: "Bart Decrem",
       position: "SVP & GM, Disney Mobile",
