9f9467232130698ac63a6546a41f17ebefa042d2
Samson Cheung <samsonc89@yahoo.com>
2023-10-03 11:19:03 -0700

    Add styling for testimonial cards

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 export const TestimonialCard = ({ item }) => {
   return (
-    <div>
+    <div className=" flex flex-col items-center">
       <div>
-        <img src={item.img} alt="" />
+        <img src={item.img} alt="" className="max-h-20" />
       </div>
-      <p>{item.text}</p>
-      <p>{item.name}</p>
-      <p>{item.position}</p>
+      <div className="px-1 text-lg  tracking-wide">
+        <p className=" my-8 font-light">{item.text}</p>
+        <p>{item.name}</p>
+        <p className="text-gray-400">{item.position}</p>
+      </div>
     </div>
   )
 }
