9878a0013c70abdbd71eecb6253b7ee50f089475
Samson Cheung <samsonc89@yahoo.com>
2023-10-03 10:43:57 -0700

    Add content copy to First testimonial

--- src/components/Mybite.jsx
+++ src/components/Mybite.jsx
@@ -1,4 +1,19 @@
 export const Mybite = () => {
+  const content = [
+    {
+      img: "",
+      text: "Yes they do beautiful design. Yes they write squeaky-clean code. But the thing that makes them so special is how darn easy they are to work with. Can you have it all? Apparently with Limitless.",
+      name: "Adam Schwartz",
+      position: "Founder, Articulate",
+    },
+    {
+      img: "",
+      text: "Limitless continues to deliver clean, elegant design—perfectly executed and always on time.",
+      name: "Bart Decrem",
+      position: "SVP & GM, Disney Mobile",
+    },
+  ]
+
   return (
     <div>
       <div>Testimonial 1</div>
