9a983a8e9299bcc99396116a1e47d5b22deef663
Samson Cheung <samsonc89@yahoo.com>
2023-10-03 11:59:32 -0700

    Fix spacing of first testimonial

--- src/components/Testimonial1.jsx
+++ src/components/Testimonial1.jsx
@@ -1,12 +1,14 @@
 import adam from "../assets/adam.webp"
 import bart from "../assets/bart.webp"
+import mybite from "../assets/mybite.webp"
+
 import { TestimonialCard } from "./TestimonialCard"
 
 export const Testimonial1 = () => {
   const content = [
     {
       img: adam,
-      text: "Yes they do beautiful design. Yes they write squeaky-clean code. But the thing that makes them so special is how darn easy they are to work with. Can you have it all? Apparently with Limitless.",
+      text: `"Yes they do beautiful design. Yes they write squeaky-clean code. But the thing that makes them so special is how darn easy they are to work with. Can you have it all? Apparently with Limitless."`,
       name: "Adam Schwartz",
       position: "Founder, Articulate",
     },
@@ -19,15 +21,25 @@ export const Testimonial1 = () => {
   ]
 
   return (
-    <div>
-      <div>
-        {content.map((item, i) => {
-          return <TestimonialCard key={i} item={item} />
-        })}
+    <section className="container">
+      <div className="flex flex-col xl:flex-row xl:gap-12">
+        <div className="flex flex-col gap-12 rounded-xl bg-white p-12   xl:flex-row">
+          {content.map((item, i) => {
+            return <TestimonialCard key={i} item={item} />
+          })}
+        </div>
+        <div className="relative mt-6 rounded-xl xl:mt-0">
+          <a href="">
+            <img src={mybite} alt="" className="" />
+          </a>
+          <div className="absolute bottom-8 left-8">
+            <p className="uppercase tracking-wider text-gray-100/70">
+              case study
+            </p>
+            <p className="text-3xl tracking-wider text-white">Mybite</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <img src="" alt="" />
-      </div>
-    </div>
+    </section>
   )
 }
--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 export const TestimonialCard = ({ item }) => {
   return (
-    <div className=" flex flex-col items-center">
+    <div className=" flex flex-col items-center text-center xl:basis-1/2 xl:items-start xl:text-left">
       <div>
         <img src={item.img} alt="" className="max-h-20" />
       </div>
       <div className="px-1 text-lg  tracking-wide">
-        <p className=" my-8 font-light">{item.text}</p>
+        <p className=" my-8 font-light xl:my-4">{item.text}</p>
         <p>{item.name}</p>
         <p className="text-gray-400">{item.position}</p>
       </div>
