409ff7fd85294d133db680f5e6f2f6dd3741e53c
Samson Cheung <samsonc89@yahoo.com>
2023-10-07 19:27:44 -0700

    Add what's included text

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -1,3 +1,5 @@
+import icon from "../assets/check-icon.svg"
+
 export const Plans = () => {
   return (
     <section className="container rounded-xl bg-white p-12 xl:py-24">
@@ -9,6 +11,39 @@ export const Plans = () => {
           Subscription pricing that fits your budget. No long-term commitments,
           cancel anytime.
         </h2>
+      </div>
+      <div>
+        <div>
+          <div></div>
+          <div>
+            <h3>What's Included</h3>
+            <div className="flex gap-2">
+              <img src={icon} />
+              <p>Unlimited email design</p>
+            </div>
+            <div className="flex gap-2">
+              <img src={icon} />
+              <p>Unlimited requests</p>
+            </div>
+            <div className="flex gap-2">
+              <img src={icon} />
+              <p>Unlimited revisions</p>
+            </div>
+            <div className="flex gap-2">
+              <img src={icon} />
+              <p>Secure project portal</p>
+            </div>
+            <div className="flex gap-2">
+              <img src={icon} />
+              <p>No meetings—everything is handled async</p>
+            </div>
+            <div className="flex gap-2">
+              <img src={icon} />
+              <p>You own the design and IP rights</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-orange-500/50"></div>
       </div>
     </section>
   )
